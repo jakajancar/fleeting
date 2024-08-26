bc87e4949bc826ddc01817c13381216e224223e9
Jaka Jančar <jaka@kubje.org>
2024-08-26 11:11:29 -0700

    Remove hardcoded ssh key

--- src/user_data_template.sh
+++ src/user_data_template.sh
@@ -10,7 +10,6 @@ echo "{{otp}}" >/fleeting/otp
 # Allow connections
 mkdir -p /root/.ssh
 echo "{{authorized_keys}}" >/root/.ssh/authorized_keys
-echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAlQWouySQLhr+MfmROC7JN+lyPNyKd4x/lEP2efgC+L jaka@kubje.org" >>/root/.ssh/authorized_keys # TODO: remove
 
 # Stay running while someone is extending the timeout
 touch /fleeting/keepalive
